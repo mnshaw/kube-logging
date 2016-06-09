d973295e879c61fb40fa95c9c64e0a14ee4b6905
Marie Shaw <mnshaw@andrew.cmu.edu>
2016-06-09 15:35:20 -0700

    First commit, script to pull log files from Google Storage

---
+++ logging.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+url=$1
+bucket=${url#h*[e][r][/]}
+bucket=${bucket%/*}
+echo $bucket
+filename=${bucket##**/}
+datetime=$(date +"%m_%d_%Y-%H_%M_%S")
+filename=$filename-$datetime
+#echo $filename
+mkdir $filename
+cd $filename
+gslink=gs://$bucket/*
+echo $gslink
+gsutil -m cp $gslink .
+
